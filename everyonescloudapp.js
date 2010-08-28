0bdcf23ba0ab9799dea200eb91aa71710a3bfc5a
Till Klampaeckel <till@php.net>
2010-08-28 17:42:28 +0200

    * openUrl  * small fixes in filter function

--- everyonescloudapp.js
+++ everyonescloudapp.js
@@ -1,4 +1,6 @@
-var sys = require('sys'), path = require('path');
+var sys = require('sys'),
+    path = require('path'),
+    exec = require('child_process').exec;
 
 exports.filter = function (files) {
     return files.filter(ignoreByPattern);
@@ -15,6 +17,19 @@ exports.validateStore = function (p) {
     }
 }
 
+exports.openUrl = function (url, browser) {
+    var foo;
+    foo = exec(browser + ' "' + url + '"', 
+        function (error, stdout, stderr) {
+            sys.print('stdout: ' + stdout);
+            sys.print('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        }
+    );
+}
+
 /**
  * This filters the array. Function is not exposed.
  *
@@ -25,41 +40,43 @@ exports.validateStore = function (p) {
 function ignoreByPattern(file) {
 
     var ignorePattern = ['.goutputstream*', '*.psd', 'test.txt'];
+    var pattern;
 
     for (var i in ignorePattern) {
 
-        if (ignorePattern[i].substr(-1, 1) == '*') {
-            //sys.puts("END: ");
+        pattern = ignorePattern[i].toString();
+
+        if (pattern.substr(-1, 1) == '*') {
 
             start = 0;
-            end   = (ignorePattern[i].length-1);
+            end   = (pattern.length-1);
 
-            sys.puts("END: " + file + " / " + ignorePattern[i]);
+            //sys.puts("END: " + file + " / " + pattern);
 
-            if (file.substr(start, end) == ignorePattern[i].substr(0, end)) {
+            if (file.substr(start, end) == pattern.substr(0, end)) {
                 return false;
             }
 
-        } else if (ignorePattern[i].substr(0, 1) == '*') {
+        } else if (pattern.substr(0, 1) == '*') {
             //sys.puts("START: ");
 
-            start = "-" + ignorePattern[i].length;
-            end   = ignorePattern[i].length;
+            start = "-" + pattern.length;
+            end   = pattern.length;
 
-            if (file.substr(start, end) == ignorePattern[i].substr(1, ignorePattern[i].length)) {
+            if (file.substr(start, end) == pattern.substr(1, pattern.length)) {
                 return false;
             }
 
         } else { // no wildcard
-            //sys.puts("NONE: " + file + " / " + ignorePattern[i]);
+            //sys.puts("NONE: " + file + " / " + pattern);
 
-            if (file == ignorePattern[i]) {
+            if (file == pattern) {
                 return false;
             }
 
         }
 
-        //sys.puts(ignorePattern[i]);
+        //sys.puts(pattern);
     }
     return true;
 }
