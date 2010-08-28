4af76921b5b5bba4c70853164a9b1314a68a6fcc
Till Klampaeckel <till@php.net>
2010-08-28 17:23:14 +0200

    progress

--- node.js
+++ node.js
@@ -1,4 +1,6 @@
-/*
+/**
+ * This nodejs script monitors a given directory. If new files appear, the public URL is displayed.
+ *
  * @author  Till Klampaeckel <till@php.net>
  * @license New BSD License
  */
@@ -7,19 +9,16 @@ var fs = require('fs'), path = require('path'), sys = require('sys');
 var cloudapp = require('./everyonescloudapp');
 var config   = require('./config');
 
-var screenshotPath = "/home/till/Dropbox/Public/screenshots/";
+var screenshotPath = config.getPath();
 
-sys.puts("Path: " + screenshotPath);
-
-fs.stat(screenshotPath, function (err, stats) {
-    if (err) {
-        throw err;
-    }
-    //console.log('stats: ' + JSON.stringify(stats));
-});
+cloudapp.validateStore(screenshotPath);
 
 var allFiles = cloudapp.filter(fs.readdirSync(screenshotPath));
 var newFiles = [];
+
+var publicUrl  = "http://dl.dropbox.com/u/";
+    publicUrl += config.dropboxUser() + "/"
+    publicUrl += path.basename(screenshotPath) + "/";
 
 /**
  * Figure out if something happened inside the directory.
@@ -34,8 +33,20 @@ fs.watchFile(screenshotPath, function (curr, prev) {
     newFiles = fs.readdirSync(screenshotPath);
     newFiles = cloudapp.filter(newFiles);
 
-    sys.puts("All: " + allFiles);
-    sys.puts("New: " + newFiles);
+    //sys.puts("All: " + allFiles);
+    //sys.puts("New: " + newFiles);
+
+    if (allFiles.compare(newFiles) == true) { 
+        return;
+    }
+
+    var reallyNew = newFiles.diff(allFiles);
+
+    sys.puts("NEW FILE(S)!");
+
+    for (var i in reallyNew) {
+        sys.puts("NEW: " + publicUrl + reallyNew[i]);
+    }
 
     //sys.puts('current mtime is: ' + curr.mtime);
     //sys.puts('previous mtime was: ' + prev.mtime);
@@ -43,3 +54,55 @@ fs.watchFile(screenshotPath, function (curr, prev) {
 
 
 /* list = fs.readdir(screenshotPath); */
+
+/* helpers */
+
+/**
+ * Allows us to check if arrays are identical.
+ *
+ * @param array testArr
+ *
+ * @return boolean
+ * @link   http://www.hunlock.com/blogs/Mastering_Javascript_Arrays
+ */
+Array.prototype.compare = function(testArr) {
+    if (this.length != testArr.length) return false;
+    for (var i = 0; i < testArr.length; i++) {
+        if (this[i].compare) { 
+            if (!this[i].compare(testArr[i])) return false;
+        }
+        if (this[i] !== testArr[i]) return false;
+    }
+    return true;
+}
+
+/**
+ * Find the difference between two arrays.
+ *
+ * @param array testArr
+ *
+ * @return array
+ */
+Array.prototype.diff = function(testArr) {
+
+    if (this.length == 0) {
+        return testArr;
+    }
+
+    var lookup = {};
+
+    for (var j in testArr) {
+        lookup[testArr[j]] = testArr[j];
+    }
+
+    var difference = [];
+
+    for (var i in this) {
+
+        if (typeof lookup[this[i]] == 'undefined') {
+            difference.push(this[i]);
+        } 
+    }
+
+    return difference;
+}
